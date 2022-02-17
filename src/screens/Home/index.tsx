6b869b2b91be2eb98b26dbc5459d0e811a524bb5
Carlos <carlosaugustodelimavieira@protonmail.com>
2022-02-16 22:20:22 -0300

    setting global styles and starting config svg components

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container } from './styles';
+
+// import { Container } from './styles';
+
+export function Home (){
+    return (
+        <Container>
+            <Text> Olá mundo! </Text>
+        </Container>
+    )
+}
+
+export default Home;
